702164ad7e61c72d9bc2a623df5505ee3a917940
zhouhao <shawock.zhou@musical.ly>
2017-12-28 00:16:55 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	_ "fmt" // 下划线让编译器接受这类导入，并且调用对应包内的所有代码文件里定义的 init 函数
+)
+
+func init() {
+	log.Println("init invoked.")
+	log.SetOutput(os.Stderr)
+}
+
+func main() {
+	log.Println("application start successfully.")
+	log.Println(length("hello shawock"))
+}
+
+func length(str string) int {
+	return len(str)
+}
---
+++ search/feed.go
@@ -0,0 +1,27 @@
+package search
+
+import (
+	"os"
+	"encoding/json"
+)
+
+const dataFile = "data/data.json"
+
+type Feed struct {
+	Name string `json:"site"`
+	URI  string `json:"link"`
+	Type string `json:"type"`
+}
+
+func RetrieveFeeds() ([]*Feed, error) {
+	file, err := os.Open(dataFile)
+	defer file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+	return feeds, err
+}
